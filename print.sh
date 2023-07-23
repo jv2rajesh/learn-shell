4528ab2fed29ab5951d28ba4527369ddfdae1546
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-23 11:44:06 +0530

    added echo in catalogue

---
+++ print.sh
@@ -0,0 +1,19 @@
+echo Hello World
+
+     # Red       31
+     # Green     32
+     # Yellow    33
+     # Blue      34
+     # Magenta   35
+     # Cyan      36
+
+     # Syntax
+     # echo -e "\e[codemMessage\e[0m"
+
+echo -e "\e[31mHellow World\e[0m"
+echo -e "\e[32mHellow World\e[0m"
+echo -e "\e[33mHellow World\e[0m"
+echo -e "\e[34mHellow World\e[0m"
+echo -e "\e[35mHellow World\e[0m"
+echo -e "\e[36mHellow World\e[0m"
+
