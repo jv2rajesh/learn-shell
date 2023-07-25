ad1ce34c93d450112758798e6ac485bf619e2bd6
Rajesh Reddy <jv2rajesh@gmail.com>
2023-07-25 21:49:03 +0530

    function

--- 4.functions.sh
+++ 4.functions.sh
@@ -7,3 +7,14 @@ greeting() {
 greeting
 
 echo func exit status $?
+
+# Function have its own special variables
+
+input() {
+  echo first argument -$1
+  echo second argument - $2
+  echo All arguments - $*
+  echo No of arguments - $#
+
+}
+input abc 1234
